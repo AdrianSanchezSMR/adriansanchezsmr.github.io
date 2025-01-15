44889aed2d7b6e097395187bd90e4f956715b6cc
AdrianSanchezDAM <148220867+AdrianSanchezSMR@users.noreply.github.com>
2025-01-15 18:30:26 +0100

    Add files via upload

---
+++ Generos_y_Sets/marcado_de_estructura.js
@@ -0,0 +1,52 @@
+
+{
+  "@context": "http://schema.org",
+  "@type": "Article",
+  "name": "Generos más destacadas",
+  "author": {
+    "@type": "Person",
+    "name": "PlayMoverso"
+  },
+  "image": "https://adriansanchezsmr.github.io/imagenes/Generos/Playmobil-de-policia.png",
+  "articleSection": "Playmobil de Policía",
+  "articleBody": "Descubre las figuras de Playmobil de policía, ideales para crear historias llenas de acción y aventura. Con detalles característicos y accesorios únicos, son perfectos para recrear escenas emocionantes. ¡Diversión garantizada para todas las edades!"
+}
+
+{
+  "@context": "http://schema.org",
+  "@type": "Article",
+  "name": "Generos más destacadas",
+  "author": {
+    "@type": "Person",
+    "name": "PlayMoverso"
+  },
+  "image": "https://adriansanchezsmr.github.io/imagenes/Generos/Playmobil-ferrari.png",
+  "articleSection": "Playmobil de Vehículos",
+  "articleBody": "Explora la variedad de vehículos Playmobil, desde autos y camiones hasta barcos y aviones. Diseñados con gran detalle y funcionalidad, son ideales para crear emocionantes aventuras sobre ruedas, agua o aire. ¡Diversión y movimiento sin límites!"
+}
+
+{
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "name": "Generos más destacadas",
+    "author": {
+      "@type": "Person",
+      "name": "PlayMoverso"
+    },
+    "image": "https://adriansanchezsmr.github.io/imagenes/Generos/Playmobil-castillo.png",
+    "articleSection": "Playmobil de Caballeros",
+    "articleBody": "Adéntrate en un mundo medieval con los Playmobil de caballeros. Crea emocionantes batallas, defiende castillos y explora la vida en un reino lleno de aventuras. Perfecto para pequeños héroes y amantes de la historia."
+  }
+
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "name": "Generos más destacadas",
+    "author": {
+      "@type": "Person",
+      "name": "PlayMoverso"
+    },
+    "image": "https://adriansanchezsmr.github.io/imagenes/Generos/Playmobil-123.png",
+    "articleSection": "Playmobil 123",
+    "articleBody": "Los Playmobil 1.2.3 son ideales para los más peques de la casa. Con diseños seguros, colores brillantes y formas fáciles de manipular, fomentan la imaginación y el desarrollo temprano mientras se divierten creando historias."
+  }
