a9a0d456c8fb2a02c9587c8ec64fad02e578dce8
AdrianSanchezDAM <148220867+AdrianSanchezSMR@users.noreply.github.com>
2025-03-23 12:55:31 +0100

    Add files via upload

---
+++ Generos_y_Sets/Playmobil_espacio/Modelos/rutaModelos.js
@@ -0,0 +1,30 @@
+{
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Inicio",
+        "item": "https://adriansanchezsmr.github.io/"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "Generos y Sets",
+        "item": "https://adriansanchezsmr.github.io/Generos_y_Sets/"
+      },
+      {
+        "@type": "ListItem",
+        "position": 3,
+        "name": "Playmobil Policía",
+        "item": "https://adriansanchezsmr.github.io/Generos_y_Sets/Playmobil_policia/"
+      },
+      {
+        "@type": "ListItem",
+        "position": 4,
+        "name": "Modelos",
+        "item": "https://adriansanchezsmr.github.io/Generos_y_Sets/Playmobil_policia/Modelos/"
+      }
+    ]
+  }
